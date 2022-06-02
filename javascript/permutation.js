bd2b1a90bd450bf2b1c2b93e4381562379a3c0d5
adeputragea <adeputragea28@gmail.com>
2022-06-02 08:20:57 +0700

    interesting medium leetcode'

---
+++ javascript/permutation.js
@@ -0,0 +1,29 @@
+function permutation(nums) {
+  let result = [];
+  let used = new Array(nums.length).fill(false);
+
+  backtracking(nums, used, result, []);
+
+  return result;
+}
+
+function backtracking(nums, used, result, temp) {
+  if (temp.length === nums.length) {
+    result.push(Array.from(temp));
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    // skip used nums
+    if (used[i]) continue;
+    // add nums to permutation, mark nums as unused
+    temp.push(nums[i]);
+    used[i] = true;
+    backtracking(nums, used, result, temp);
+    // remove nums from permutation, mark nums as unused
+    temp.pop();
+    used[i] = false;
+  }
+}
+
+// console.log(permutation([1, 2, 3]));
+console.log(permutation([0, 1]));
